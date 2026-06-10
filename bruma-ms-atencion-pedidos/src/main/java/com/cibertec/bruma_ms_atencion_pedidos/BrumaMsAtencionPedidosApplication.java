ec0239894595bf9ab4ba96a55dbc46ec2e107ceb
Rlizanov <omarlivill@gmail.com>
2026-06-09 23:59:13 -0500

    implementa entidades Producto, Mesa y Empleado.

---
+++ bruma-ms-atencion-pedidos/src/main/java/com/cibertec/bruma_ms_atencion_pedidos/BrumaMsAtencionPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.bruma_ms_atencion_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrumaMsAtencionPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrumaMsAtencionPedidosApplication.class, args);
+	}
+
+}
