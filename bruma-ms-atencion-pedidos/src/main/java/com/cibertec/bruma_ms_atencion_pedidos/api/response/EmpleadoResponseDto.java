2ee1428835eaedd75f0cab56aafd52befa0f82f8
Rlizanov <omarlivill@gmail.com>
2026-06-20 00:41:48 -0500

    incluir fecha_ingreso y apellido materno en EmpleadoResponseDto

--- bruma-ms-atencion-pedidos/src/main/java/com/cibertec/bruma_ms_atencion_pedidos/api/response/EmpleadoResponseDto.java
+++ bruma-ms-atencion-pedidos/src/main/java/com/cibertec/bruma_ms_atencion_pedidos/api/response/EmpleadoResponseDto.java
@@ -2,12 +2,17 @@ package com.cibertec.bruma_ms_atencion_pedidos.api.response;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class EmpleadoResponseDto {
     private Long id;
     private String nombre;
     private String apellidoPaterno;
+    private String apellidoMaterno;
     private String dni;
     private String email;
     private String cargo;
+    private LocalDate fechaIngreso;
+    private Boolean estado;
 }
