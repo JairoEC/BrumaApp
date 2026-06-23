e5fa0bb469e7cdb59ff8b153d1f5e17134d36c08
Jennifer <jackcorrea49@gmail.com>
2026-06-23 14:46:05 -0500

    ocultar el navbar en la vista de login

--- bruma-frontend/src/app/app.component.ts
+++ bruma-frontend/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  // Solo dejamos las herramientas de rutas que usa el menú (navbar):
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'bruma-frontend';
+
+  // 1. Inyectamos el Router en el constructor
+  constructor(private router: Router) {}
+
+  // 2. Creamos la función que valida si estamos en el Login
+  esRutaLogin(): boolean {
+    return this.router.url === '/login' || this.router.url === '/';
+  }
 }
